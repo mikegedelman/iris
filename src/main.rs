f9ed404cc53a3e95ff4493292c6423b39db91ba2
Mike Gedelman <mgedelman@aledade.com>
2021-02-09 17:05:48 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
 
 mod parse;
 mod run;
 
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
 
 fn main() {
-    let unparsed_file = std::fs::read_to_string("main.iris")
-        .expect("cannot read iris file");
+    // let unparsed_file = std::fs::read_to_string("main.iris")
+    //     .expect("cannot read iris file");
 
-    let ast = parse::parse(&unparsed_file).expect("unsuccessful parse");
-    // println!("{:?}", ast);
+    // let ast = parse::parse(&unparsed_file).expect("unsuccessful parse");
+    // // println!("{:?}", ast);
 
-    run::run(ast);
+    // run::run(ast);
+    // assert!(grammar::TermParser::new().parse("22").is_ok());
+    println!("hi");
 }
